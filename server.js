16ed75a094e4726761103b8e78bd1511f7acf4c0
taimooraslam1 <taimoor.aslam@invozone.com>
2022-04-06 21:37:43 +0500

    express js routing

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const friends = [
+  { id: 0, name: "Taimoor" },
+  { id: 1, name: "Talha" },
+  { id: 2, name: "Junaid" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.get("/user", (req, res) => {
-  res.send("<ul><li>Hello Taimoor!</li></ul>");
+  res.json(friends);
 });
 app.post("/user", (req, res) => {
   console.log("Posting");
+});
+
+app.get("/user/:userId", (req, res) => {
+  const userId = req.params.userId;
+  const user = friends[userId];
+  user
+    ? res.status(200).json(user)
+    : res.status(404).json({ error: "User not found" });
 });
 
 app.listen(port, () => {
